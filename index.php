152fab6894456baa99b3a9bbb6ba2e01c1fb85cb
Steven Harradine <stevenharradine@gmail.com>
2016-07-26 17:03:27 -0400

    sanitize the data

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php
-  echo '{"tls_version": "' . $_SERVER['X-HTTPS-Protocol'] . '"}';
+  $unclean_tls_version = $_SERVER['X-HTTPS-Protocol'];
+  $tls_version = '';
+
+  // sanitize tls version
+  if (
+    $unclean_tls_version == 'TLS1.2' ||
+    $unclean_tls_version == 'TLSv1.1' ||
+    $unclean_tls_version == 'TLSv1'
+  ) {
+    $tls_version = $unclean_tls_version;
+  } else {
+    $tls_version = "Sanitization failed"
+  }
+
+  echo "{\"tls_version\": \"$tls_version\"}";
